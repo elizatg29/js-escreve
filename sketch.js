67100e2ffc553c182f59aa98bc0c8fe64317a600
elizatg29 <elizatorrezang@gmail.com>
2024-05-23 09:01:46 -0300

    aula chata

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(700, 700);
+    background("white");
+  }
+  
+  function draw() {
+    
+    stroke("green");
+    fill("green");
+  
+    // console.log (mouseIsPressed);
+  
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 20);
+    }
+  }
